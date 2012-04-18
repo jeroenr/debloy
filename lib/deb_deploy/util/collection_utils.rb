22430b45e5c94d6998da71fd0c8cbeb45a0bd71b
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2012-04-18 21:13:41 +0200

    Used yield

--- lib/deb_deploy/util/collection_utils.rb
+++ lib/deb_deploy/util/collection_utils.rb
@@ -3,25 +3,25 @@ module DebDeploy
     def each(&block)
       threads = []
       super do |item|
-        threads << Thread.new { block.call(item) }
+        threads << Thread.new { yield item }
       end
       threads.each(&:join)
     end
 
     def map(&block)
       super do |item|
-        Thread.new { Thread.current[:output] = block.call(item) }
-      end.map(&:join).map { |t| t[:output] }
+        Thread.new { Thread.current[:output] = block[item] }
+      end.map(&:join).map { |thread| thread[:output] }
     end
   end
 
   module CollectionUtils
-    def self.async(synced_collection)
-      asynced_collection = synced_collection.clone
+    def self.async(synchronous_collection)
+      CollectionUtils.convert_to_async_collection(synchronous_collection.clone)
     end
 
-    def self.async!(synced_collection)
-
+    def self.async!(synchronous_collection)
+      CollectionUtils.convert_to_async_collection(synchronous_collection)
     end
 
     private def self.convert_to_async_collection(collection)
