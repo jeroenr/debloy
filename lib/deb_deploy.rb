3a97b798df69ba6c5090da07009e5a685984c8b6
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2012-04-18 11:03:55 +0200

    Fixed rsync command to include only deb packages

--- lib/deb_deploy.rb
+++ lib/deb_deploy.rb
@@ -15,7 +15,7 @@ Capistrano::Configuration.instance.load do
   		failed_targets = targets.map do |target|
   			copy_cmd = DebDeploy::Rsync.command(
   				debian_source,
-  				DebDeploy::Rsync.remote_address(server.user || fetch(:user, ENV['USER']), server.host, debian_target),
+  				DebDeploy::Rsync.remote_address(target.user || fetch(:user, ENV['USER']), target.host, debian_target),
   				:ssh => { 
   					:keys => ssh_options[:keys], 
   					:config => ssh_options[:config], 
@@ -23,14 +23,18 @@ Capistrano::Configuration.instance.load do
   				}
   			)
   			logger.debug copy_cmd
-  			server.host unless system copy_cmd
+  			target.host unless system copy_cmd
   		end.compact
 
   		raise "rsync failed on #{failed_targets.join(',')}" if failed_targets.any?
     end
 
     task :install_packages do
-    	log = if debian_stream_log DebDeploy::Logger::Stream.new(logger) else DebDeploy::Logger::Batch.new(logger) end
+    	log = if debian_stream_log 
+		DebDeploy::Logger::Stream.new(logger) 
+	      else 
+		DebDeploy::Logger::Batch.new(logger) 
+	      end
 
 	    begin
 	      run "#{sudo} #{debian_package_manager} -R -i #{debian_target}" do |channel, stream, data|
